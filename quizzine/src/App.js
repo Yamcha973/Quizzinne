ec265dd51d48b1f86fb52c868cc23a83d2e6c562
Yamcha973 <jessica_mds_7@hotmail.com>
2020-04-17 10:56:17 +0200

    footer

--- quizzine/src/App.js
+++ quizzine/src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App"> 
-    <Footer />
-    
+    <div className="App">
+      <Footer />
     </div>
   );
 }
--- quizzine/src/components/Footer.js
+++ quizzine/src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import './Footer.css'; 
+import './Footer.css';
+import {ReactComponent as Facebook} from '../facebook_social_media_icon_127370.svg';
+import {ReactComponent as Instagram} from '../instagram_social_media_icon_127363.svg';
+import {ReactComponent as Twitter} from '../twitter_social_media_icon_127368.svg';
+import {ReactComponent as Youtube} from '../youtube_social_media_icon_127358.svg';
 import { Table } from 'reactstrap';
-
+ 
 const Footer = (props) => {
   return (
     <Table borderless>
@@ -9,8 +13,13 @@ const Footer = (props) => {
         <tr>
           <th><a href='F.A.Q'>F.A.Q</a></th>
           <th><a href='Contact'>Contact</a></th>
-          <th>Last Name</th>
-          <th>www.Quizzine.com</th>
+          <div className="icons">
+          <th><Instagram /></th>
+          <th><Youtube /></th>
+          <th><Facebook /></th>
+          <th><Twitter /></th>
+          </div>
+          <th><a href='www.Quizzine.com©'>www.Quizzine.com©</a> </th>
         </tr>
       </thead>
     </Table>
