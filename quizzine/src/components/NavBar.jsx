5138094d2a5aebfd87507a72a0a60a059ab14d43
gtn777 <gtn777@orange.fr>
2020-05-11 09:46:27 +0200

    update navbar

--- quizzine/src/components/NavBar.jsx
+++ quizzine/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NavBarStyle.css";
 import logo from "./logo.png";
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
 class NavBar extends React.Component{
     constructor(props) {
@@ -13,16 +14,19 @@ class NavBar extends React.Component{
     render() {
         return(                 
         <div className="gui-navBar" >
-            <img src={logo} alt="Quizzine, le quizz des fins gourmets!"/>
-            <ul>                
-                {this.state.isAuthenticated ? <li>Démarrer un Quizz</li>: <li>Quizz aléatoire</li> }
-                {this.state.isAuthenticated ? <li>Défier un membre</li> : <li>Pourquoi s'inscrire?</li> }
-                {this.state.isAuthenticated ? <li>Profil et resultats</li> : <li>Créer un compte</li>  }
+            
+             
+            <ul>
+                <li><Link to='/' >Accueil</Link></li>                
+                {this.state.isAuthenticated ? <li><Link to='quizz-start' >Démarrer un Quizz</Link></li>: <li>Quizz aléatoire</li> }
+                {this.state.isAuthenticated ? <li>Quizz aléatoire</li> : <li>Pourquoi s'inscrire?</li> }
+                {this.state.isAuthenticated ? <li><Link to='profile' >Profil et resultats</Link></li> : <li><Link to='form' >Créer un compte</Link></li> }
                 <li onClick={event => {
                 const newStatus = !this.state.isAuthenticated;
                 this.setState({ isAuthenticated: newStatus });}}
                  className="authentification">{this.state.isAuthenticated ? "Déconnexion" : "Connexion" }</li>                
             </ul>
+           
         </div>
         );
     }
